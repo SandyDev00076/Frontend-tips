f1bafc5366240ad34bade272627102f4ab800d8f
Sanjeet Tiwari <sanjeettiwari00076@gmail.com>
2021-10-24 01:42:36 +0530

    Add project structure

---
+++ src/components/GenreList.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import styles from "./GenreList.module.scss";
+
+const GenreList = () => {
+  return <section className={styles.container}>GenreList</section>;
+};
+
+export default GenreList;

c393e427f54f756085606aeeaf02a3dba7d41f24
Sanjeet Tiwari <sanjeettiwari00076@gmail.com>
2021-10-25 02:11:57 +0530

    remove unused export

--- src/components/GenreTile.tsx
+++ src/components/GenreTile.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { motion } from "framer-motion";
 import { Genre } from "types/Genre";
-import { getRandomArbitrary } from "utils";
 
 import styles from "./GenreTile.module.scss";
 

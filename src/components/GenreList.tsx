85c8bf09d1fb4f0248c674a4edbd70724b5d8456
Sanjeet Tiwari <sanjeettiwari00076@gmail.com>
2021-10-25 01:37:28 +0530

    add framer motion and pretty basic anims

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,21 +1,38 @@
 import React from "react";
 import { genreList } from "data/GenreList";
 import GenreTile from "./GenreTile";
+import { motion } from "framer-motion";
 
 import styles from "./GenreList.module.scss";
+
+const popFromLeft = {
+  initial: {
+    opacity: 0,
+    x: -20,
+  },
+  animate: {
+    opacity: 1,
+    x: 0,
+  },
+};
 
 const GenreList = () => {
   return (
     <section className={styles.container}>
       <div className={styles.intro}>
-        <h1>need some help?</h1>
-        <h2>
+        <motion.h1 {...popFromLeft}>need some help?</motion.h1>
+        <motion.h2
+          {...popFromLeft}
+          transition={{
+            delay: 0.4,
+          }}
+        >
           Pick a <strong>Genre</strong>
-        </h2>
+        </motion.h2>
       </div>
       <div className={styles.genres}>
-        {genreList.map((genre) => (
-          <GenreTile obj={genre} key={genre.id} />
+        {genreList.map((genre, index) => (
+          <GenreTile obj={genre} key={genre.id} index={index} />
         ))}
       </div>
     </section>
--- src/components/GenreTile.tsx
+++ src/components/GenreTile.tsx
@@ -1,13 +1,33 @@
 import React from "react";
+import { motion } from "framer-motion";
 import { Genre } from "types/Genre";
 
 import styles from "./GenreTile.module.scss";
 
 interface Props {
   obj: Genre;
+  index: number;
 }
-const GenreTile = ({ obj }: Props) => {
-  return <button className={styles.container}>{obj.title}</button>;
+const GenreTile = ({ obj, index }: Props) => {
+  return (
+    <motion.button
+      initial={{
+        opacity: 0,
+      }}
+      animate={{
+        opacity: 1,
+      }}
+      transition={{
+        delay: 0.4 * index + 0.8,
+      }}
+      whileHover={{
+        scale: 1.05,
+      }}
+      className={styles.container}
+    >
+      {obj.title}
+    </motion.button>
+  );
 };
 
 export default GenreTile;

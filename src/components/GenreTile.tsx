15ba81ccd893f9332d7d260ff8a0cf0f99cc395e
Sanjeet Tiwari <sanjeettiwari00076@gmail.com>
2021-10-26 00:53:00 +0530

    convert btns into links

--- src/components/GenreTile.tsx
+++ src/components/GenreTile.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { motion } from "framer-motion";
 import { Genre } from "types/Genre";
 import { NO_OF_VARIANTS } from "App.constants";
+import { Link } from "react-router-dom";
 
 import styles from "./GenreTile.module.scss";
 
@@ -9,26 +10,44 @@ interface Props {
   obj: Genre;
   index: number;
 }
+
 const GenreTile = ({ obj, index }: Props) => {
+  const [focused, setFocused] = useState(false);
+
+  const variants = {
+    appear: {
+      opacity: 1,
+      transition: {
+        delay: 0.3 * index + 1.2,
+      },
+    },
+    focus: {
+      filter: focused ? "brightness(0.9)" : "unset",
+    },
+  };
+
   const variantNumber = (index % NO_OF_VARIANTS) + 1;
   return (
-    <motion.button
-      initial={{
-        opacity: 0,
-      }}
-      animate={{
-        opacity: 1,
-        transition: {
-          delay: 0.3 * index + 1.2,
-        },
-      }}
-      whileHover={{
-        y: -3,
-      }}
-      className={`${styles.container} variant${variantNumber}`}
+    <Link
+      to={`genre/${obj.id}`}
+      className={styles.link}
+      onFocus={() => setFocused(true)}
+      onBlur={() => setFocused(false)}
     >
-      {obj.title}
-    </motion.button>
+      <motion.div
+        initial={{
+          opacity: 0,
+        }}
+        variants={variants}
+        animate={["appear", "focus"]}
+        whileHover={{
+          y: -3,
+        }}
+        className={`${styles.container} variant${variantNumber}`}
+      >
+        {obj.title}
+      </motion.div>
+    </Link>
   );
 };
 

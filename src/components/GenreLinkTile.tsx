a911d21587608614c481b28d97357740e6a98d9b
Sanjeet Tiwari <sanjeettiwari00076@gmail.com>
2021-10-28 02:29:23 +0530

    keep it simple

--- src/components/GenreLinkTile.tsx
+++ src/components/GenreLinkTile.tsx
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import { GenreLink } from "types/GenreLink";
 import { prepareURL, truncateURL } from "utils";
 import TileContainer from "./TileContainer";
-import { motion, AnimatePresence } from "framer-motion";
 
 import styles from "./GenreLinkTile.module.scss";
 
@@ -12,7 +11,6 @@ interface Props {
 }
 const GenreLinkTile = ({ obj, index }: Props) => {
   const [focused, setFocused] = useState(false);
-  const [descVisibility, showDesc] = useState(false);
 
   return (
     <a
@@ -27,24 +25,10 @@ const GenreLinkTile = ({ obj, index }: Props) => {
         tileIndex={index}
         className={styles.container}
         focused={focused}
-        onHoverStart={() => showDesc(true)}
-        onHoverEnd={() => showDesc(false)}
       >
         <div className={styles.title}>{obj.title}</div>
         <div className={styles.url}>{truncateURL(obj.link)}</div>
       </TileContainer>
-      <AnimatePresence>
-        {descVisibility && (
-          <motion.div
-            className={styles.desc}
-            initial={{ y: 10, opacity: 0 }}
-            animate={{ y: 0, opacity: 1 }}
-            exit={{ y: 10, opacity: 0 }}
-          >
-            {obj.description}
-          </motion.div>
-        )}
-      </AnimatePresence>
     </a>
   );
 };
